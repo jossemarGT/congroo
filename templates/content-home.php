941770e3237b42b0ef711d939b82eec636541ba9
jossemarGT <jossemargt@gmail.com>
2014-11-07 16:04:15 -0600

    New styles

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,11 +1,16 @@
 <article <?php post_class('fade-effect'); ?>>
-  <header class="header excerpt-header">
+  <header class="excerpt-header">
     <img class="bg-img"
     src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' )[0]; ?>"
     >
-    <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-    <?php get_template_part('templates/entry-meta'); ?>
-    <a href="<?php the_permalink(); ?>"><span class="sr-only"><?php _e('Read more'); ?></span><a>
+    <div class="header-body">
+      <h2 class="entry-title"><?php the_title(); ?></h2>
+      <?php get_template_part('templates/entry-meta'); ?>
+      <a class="read-more" href="<?php the_permalink(); ?>" title="Read more">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only"><?php _e('Read more'); ?></span>
+      </a>
+    </div>
   </header>
   <div class="entry-summary sr-only">
     <?php the_excerpt(); ?>
