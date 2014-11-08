12d1581e45e80759bdf7612e9c9eb14c9d711735
jossemarGT <jossemargt@gmail.com>
2014-11-07 21:52:25 -0600

    New styles +1

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,4 +1,4 @@
-<article <?php post_class('fade-effect'); ?>>
+<article <?php post_class(get_post_meta($post->ID, 'post-thumbnail-bg-tone')); ?>>
   <header class="excerpt-header">
     <img class="bg-img"
     src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' )[0]; ?>"
@@ -6,9 +6,17 @@
     <div class="header-body">
       <h2 class="entry-title"><?php the_title(); ?></h2>
       <?php get_template_part('templates/entry-meta'); ?>
-      <a class="read-more" href="<?php the_permalink(); ?>" title="Read more">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only"><?php _e('Read more'); ?></span>
+      <a class="round-link share-facebook" href="<?php the_permalink(); ?>" title="<?php _e('Share this post', 'roots'); ?>">
+        <span class="icon glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only"><?php _e('Share this post', 'roots'); ?></span>
+      </a>
+      <a class="round-link read-more" href="<?php the_permalink(); ?>" title="<?php _e('Read more', 'roots'); ?>">
+        <span class="icon glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only"><?php _e('Read more', 'roots'); ?></span>
+      </a>
+      <a class="round-link share-facebook" href="<?php the_permalink(); ?>" title="<?php _e('Tweet this post', 'roots'); ?>">
+        <span class="icon glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only"><?php _e('Tweet this post', 'roots'); ?></span>
       </a>
     </div>
   </header>
